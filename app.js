8987aec4122bc15e439f6dd33185673816fac16b
Antly74 <antly74@gmail.com>
2022-08-17 22:43:52 +0500

    еще одна ошибка автотестов

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.get('*', (req, res) => {
+app.all('*', (req, res) => {
   res.status(404).send({ message: 'Такого маршрута не существует' });
 });
 
